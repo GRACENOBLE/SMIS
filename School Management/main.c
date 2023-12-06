9833d5b215b468167e7dd0b6a5212487ebc1517f
GRACENOBLE <gracenoble72@gmail.com>
2023-12-06 10:05:35 +0300

    Start menu
    Adding start menu where an individual chooses a give functionality of the School management information syatem

--- School Management/main.c
+++ School Management/main.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void add_Student(){
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("\t\t\t~~~~~SMIS PORTAL~~~~~\t\t\n\n1.Add student details.\t\t\t\t2.Find student by name or roll number.\n\n3.Find students registered in course.\t\t4.Count of students.\n\n5.Delete student\t\t\t\t6.Update student\n\n");
+
     return 0;
 }
