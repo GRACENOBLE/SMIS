6002ac732d2698f19ecacd4d0a5eb821481b774b
GRACENOBLE <gracenoble72@gmail.com>
2023-12-06 14:03:21 +0300

    adding student info
    My perspective on the addition of a students, this program does not save to memory yet so the student info is volatile

--- School Management/main.c
+++ School Management/main.c
@@ -1,8 +1,52 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct student{
+    char Student_ID[10];
+    char Full_Name[20];
+    char Date_of_Birth[10];
+    char Gender[6];
+    char Contact_Information[15];
+} student;
+
+student students[];
+int index = sizeof(student);
+
 void add_Student(){
-    printf("\nComming soon\n");
+    printf("\nPlease input student details:");
+
+    printf("\nStudent_ID:\t");
+    scanf("%s", &students[index].Student_ID);
+
+    printf("\nFull_Name:\t");
+    scanf("%s", &students[index].Full_Name);
+
+    printf("\nDate_of_Birth:\t");
+    scanf("%s", &students[index].Date_of_Birth);
+
+    printf("\nGender:\t");
+    scanf("%s", &students[index].Gender);
+
+    printf("\nContact_Information:\t");
+    scanf("%s", &students[index].Contact_Information);
+
+    printf("\n\nStudent successfully added.\n\n1.Add another student\n2.Go back to the main menu\n3.Exit the program");
+    int choice;
+    scanf( "%d" , &choice );
+    switch(choice){
+case 1:
+    add_Student();
+    break;
+case 2:
+    main();
+    break;
+default:
+    return 0;
+    break;
+
+
+    }
+
 }
 
 void find_Student_By_Name_Or_Roll_Number(){
