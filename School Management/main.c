4aa96932ee6f8bda5ce2d9b4c1763c153c3ecd57
GRACENOBLE <gracenoble72@gmail.com>
2023-12-06 10:43:43 +0300

    Adding menu options
    adding functions for doing different tasks

--- School Management/main.c
+++ School Management/main.c
@@ -5,6 +5,26 @@ void add_Student(){
 
 }
 
+void find_Student_By_Name_Or_Roll_Number(){
+
+}
+
+void find_Students_Registered_In_Course(){
+
+}
+
+void count_Of_Students(){
+
+}
+
+void delete_Student(){
+
+}
+
+void update_Student(){
+
+}
+
 int main()
 {
     printf("\t\t\t~~~~~SMIS PORTAL~~~~~\t\t\n\n1.Add student details.\t\t\t\t2.Find student by name or roll number.\n\n3.Find students registered in course.\t\t4.Count of students.\n\n5.Delete student\t\t\t\t6.Update student\n\n");
